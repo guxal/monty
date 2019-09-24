9f7c33eb52e01d2f2b6d9e736d83d426fcb9524e
Drose <davidroserocalle@gmail.com>
2019-09-24 23:31:33 +0000

    add header file with the structs and main file, create variables and verify if the argc is diferent with 2

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+int main(int argc, char **argv)
+{
+	char *buf = NULL;
+	chat *filename;
+	size_t buf_size = 0;
+
+	if (argc != 2)
+	{
+		printf("Error: no such file\n");
+		error(1);
+	}
+}
+
