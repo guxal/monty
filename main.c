80e9e38d9b559d8457d69bb9ea58fbe1cc5fe304
Drose <davidroserocalle@gmail.com>
2019-09-25 21:51:17 +0000

    remove the function push and add variable to get the linenum

--- main.c
+++ main.c
@@ -1,18 +1,6 @@
 #include "monty.h"
 
 /**
- * _push - add new node in the beginnig of the stack.
- * @stack: head of stack (double linked list).
- * @line_number: the number of the line.
- */
-
-void _push(stack_t **stack, unsigned int line_number)
-{
-	(void)line_number;
-	
-}
-
-/**
  * _pall - prints all the values on the stack
  * @stack: head of stack (double linked list).
  * @line_number: the number of the line.
@@ -36,6 +24,7 @@ int main(int argc, char **argv)
 	size_t buf_size = 0;
 	FILE *file;
 	int i = 0, len = 0;
+	unsigned int linenum = 0;
 	instruction_t func[] = { {"push", _push}, {"pall", _pall}, {NULL, NULL}};
 	stack_t *stack = NULL;
 
@@ -73,7 +62,7 @@ int main(int argc, char **argv)
 	while (getline(&buf, &buf_size, file) > 0)
 	{
 		new = buf;
-
+		linenum++;
 		len = strlen(buf);
 		while ((new[i] == delim[0]) || (new[i] == delim[1]))
 			i++;
@@ -82,14 +71,13 @@ int main(int argc, char **argv)
 
 		dba->input[0] = strtok(buf, delim);
 		dba->input[1] = strtok(NULL, delim);
-		printf("%s %s\n", dba->input[0], dba->input[1]);
 
 		i = 0;
 		while (func[i].opcode != NULL)
 		{
 			if (strncmp(func[i].opcode, dba->input[0], strlen(dba->input[0])) == 0)
 			{
-				func[i].f(&stack, 1);
+				func[i].f(&stack, linenum);
 				break;
 			}
 			i++;
