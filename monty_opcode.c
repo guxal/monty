e0b6e39111f05046d0ad042cf6c199b5a923d799
Drose <davidroserocalle@gmail.com>
2019-09-25 22:13:20 +0000

    create the function add_nodeint and using in push function

--- monty_opcode.c
+++ monty_opcode.c
@@ -8,24 +8,37 @@
 
 void _push(stack_t **stack, unsigned int line_number)
 {
-        char *num = NULL;
-        stack_t *new = NULL;
+	char *num = NULL;
+	stack_t *new = NULL;
 
-        if (dba->input[1] == NULL)
-                printf("Error push NULL\n");
-        else
-                num = dba->input[1];
+	if (dba->input[1] == NULL)
+		printf("Error push NULL\n");
+	else
+		num = dba->input[1];
 
-        if (is_number(num) == 0)
-                printf("L%d: usage: push integer\n", line_number);
-        new = malloc(sizeof(stack_t));
-        if (!new)
-                perror("malloc:");
+	if (is_number(num) == 0)
+		printf("L%d: usage: push integer\n", line_number);
+	new = malloc(sizeof(stack_t));
+	if (!new)
+		perror("malloc:");
+	
+	new->n = atoi(num);
+	add_dnodeint(stack, new);
+}
 
-        new->n = atoi(num);
-        new->prev = NULL;
-        new->next = *stack;
-        if (*stack)
-                (*stack)->prev = new;
-        *stack = new;
+/**
+ * add_dnodeint - Add node in the first position
+ * @stack: the header of de double linked list
+ * @new: new
+ *
+ * Return: double linked list
+ */
+
+void add_dnodeint(stack_t **stack, stack_t *new)
+{
+	new->prev = NULL;
+	new->next = *stack;
+	if (*stack)
+		(*stack)->prev = new;
+	*stack = new;
 }
