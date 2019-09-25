fe9b493cca8a32321872966fc0e14a8888b8d31f
Drose <davidroserocalle@gmail.com>
2019-09-25 16:17:57 +0000

    add more pointers, create the delimiter and create the double pointer to contaign the trsings

--- main.c
+++ main.c
@@ -10,15 +10,17 @@
 
 int main(int argc, char **argv)
 {
-	char *buf = NULL;
-	char *filename;
+	char *buf = NULL, *filename = NULL, *delim = NULL, *new = NULL;
+	char **copy;
 	size_t buf_size = 0;
 	FILE *file;
+	int i = 0, len = 0;
 
+	delim = "\n ";
 	if (argc != 2)
 	{
 		perror("Error: no such file\n");
-		return(1);
+		return (1);
 	}
 
 	filename = argv[argc - 1];
@@ -29,9 +31,26 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-	while (getline(&buf, &buf_size, file) > 0)
-		printf("%s", buf);
+	copy = malloc(sizeof(char *) * 3);
+	if (!copy)
+	{
+		perror("malloc");
+		return (1);
+	}
 
+	while (getline(&buf, &buf_size, file) > 0)
+	{
+		new = buf;
+
+		len = strlen(buf);
+		while ((new[i] == delim[0]) || (new[i] == delim[1]))
+			i++;
+		if (i == len)
+			continue;
+
+		copy[0] = strtok(buf, delim);
+		copy[1] = strtok(NULL, buf);
+	}
+	printf("%s\n%s", copy[0], copy[1]);
 	return (0);
 }
-
