89fd8973bd1361ee30a64a7c6f3fd8d8daaf723e
JonnyCash <jonathanacp93@gmail.com>
2019-09-26 13:35:23 -0500

    update betty styles

--- monty_opcode.c
+++ monty_opcode.c
@@ -1,11 +1,9 @@
 #include "monty.h"
-
 /**
  * _push - add new node in the beginnig of the stack.
  * @stack: head of stack (double linked list).
  * @line_number: the number of the line.
  */
-
 void _push(stack_t **stack, unsigned int line_number)
 {
 	char *num = NULL;
@@ -24,11 +22,10 @@ void _push(stack_t **stack, unsigned int line_number)
 	new = malloc(sizeof(stack_t));
 	if (!new)
 		perror("malloc:");
-	
+
 	new->n = atoi(num);
 	add_dnodeint(stack, new);
 }
-
 /**
  * add_dnodeint - Add node in the first position
  * @stack: the header of de double linked list
@@ -36,7 +33,6 @@ void _push(stack_t **stack, unsigned int line_number)
  *
  * Return: double linked list
  */
-
 void add_dnodeint(stack_t **stack, stack_t *new)
 {
 	new->prev = NULL;
@@ -45,21 +41,19 @@ void add_dnodeint(stack_t **stack, stack_t *new)
 		(*stack)->prev = new;
 	*stack = new;
 }
-
 /**
  * _pall - prints all the values on the stack
  * @stack: head of stack (double linked list).
  * @line_number: the number of the line.
  */
-
 void _pall(stack_t **stack, unsigned int line_number)
 {
-        stack_t *tmp = NULL;
+	stack_t *tmp = NULL;
 
-        tmp = *stack;
-        while (tmp)
-        {
-                printf("%d\n", tmp->n);
-                tmp = tmp->next;
-        }
+	tmp = *stack;
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 }
