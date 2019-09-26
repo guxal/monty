871357e71345bf3458b3c1ebc11327a8bdc5af40
JonnyCash <jonathanacp93@gmail.com>
2019-09-26 13:33:35 -0500

    update betty styles

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include "monty.h"
-
-/* implementation of myStartupFun */
-void myStartupFun (void)
+/**
+ * myStartupFun - construct init function
+ */
+void myStartupFun(void)
 {
 	instruction_t func[] = {{"push", _push}, {"pall", _pall}, {NULL, NULL}};
 
 	dba = malloc(sizeof(dba_t));
-	if(!dba)
+	if (!dba)
 		exit(1);
 
 	dba->func = malloc((sizeof(instruction_t) * 3) + 1);
@@ -16,7 +17,7 @@ void myStartupFun (void)
 	if (!dba->input)
 	{
 		perror("malloc");
-		exit (1);
+		exit(1);
 	}
 }
 /**
@@ -29,57 +30,44 @@ void myStartupFun (void)
 
 int main(int argc, char **argv)
 {
-	char *buf = NULL, *filename = NULL, *delim = NULL, *new = NULL;
+	char *buf = NULL, *filename = NULL, *delim = "\n ", *new = NULL;
 	size_t buf_size = 0;
 	FILE *file;
 	int i = 0, len = 0;
 	unsigned int linenum = 0;
 	stack_t *stack = NULL;
 
-	delim = "\n ";
-
 	if (argc != 2)
 	{
 		perror("Error: no such file\n");
 		return (1);
 	}
-
 	filename = argv[argc - 1];
 	file = fopen(filename, "r");
-
 	if (file == NULL)
 	{
 		perror("Error: can't open file\n");
 		return (1);
 	}
-
-
-
 	while (getline(&buf, &buf_size, file) > 0)
 	{
-		new = buf;
+		new = buf, len = strlen(buf);
 		linenum++;
-		len = strlen(buf);
 		while ((new[i] == delim[0]) || (new[i] == delim[1]))
 			i++;
 		if (i == len)
 			continue;
-
 		dba->input[0] = strtok(buf, delim);
 		dba->input[1] = strtok(NULL, delim);
 		i = 0;
-		//printf("%s", dba->func[3].opcode);
 		while (dba->func[i].opcode != NULL)
 		{
 			if (strncmp(dba->func[i].opcode, dba->input[0], strlen(dba->input[0])) == 0)
 			{
 				dba->func[i].f(&stack, linenum);
 				break;
-			}
-			i++;
+			} i++;
 		}
-	}
-
-	free(dba->input);
+	} free(dba->input);
 	return (0);
 }
