958910592b6f1924ebd2cf3e0b03a30751a60344
JonnyCash <jonathanacp93@gmail.com>
2019-09-26 23:05:32 -0500

    add new function opcode

--- monty_opcode1.c
+++ monty_opcode1.c
@@ -11,3 +11,24 @@ void _nop(stack_t **stack, unsigned int line_number)
 	(void) stack;
 	(void) line_number;
 }
+
+/**
+ * _add - add two numbers from the top of that stack
+ * @stack: the stack
+ * @line_number: the line number
+ * Return: none
+ **/
+void _add(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	sum = (*stack)->n + (*stack)->next->n;
+	_pop(stack, line_number);
+	(*stack)->n = sum;
+}
