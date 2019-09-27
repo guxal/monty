8b78b5606a4f3ec6c181a79caa67896705c93e89
Drose <davidroserocalle@gmail.com>
2019-09-27 01:55:11 +0000

    add the comand nop and add the prototype

---
+++ monty_opcode1.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn’t do anything.
+ * @stack: the double pointer.
+ * @line_number: the line of command
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
