b0010315b320c8e49d8964e6d7997ca29f4b8644
JonnyCash <jonathanacp93@gmail.com>
2019-09-27 00:02:21 -0500

    add newline in the errors

--- monty_opcode1.c
+++ monty_opcode1.c
@@ -63,13 +63,13 @@ void _div(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	if ((*stack)->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zero", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
