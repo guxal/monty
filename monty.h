105f301680a295a9e6407bfe0b40310b0397c11f
JonnyCash <jonathanacp93@gmail.com>
2019-09-26 23:05:15 -0500

    add new prototype

--- monty.h
+++ monty.h
@@ -69,12 +69,14 @@ dba_t *dba;
 int is_number(char *num);
 
 /*--- add in stack ---*/
-void _push(stack_t **, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+
 void add_dnodeint(stack_t **, stack_t *);
 
 void free_all(stack_t **stack, FILE *file);
