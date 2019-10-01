7220aa954181ac1d2c0899954c664e16786c2320
JonnyCash <jonathanacp93@gmail.com>
2019-09-30 19:56:21 -0500

    hotfix

--- monty_opcode2.c
+++ monty_opcode2.c
@@ -35,12 +35,12 @@ void _mod(stack_t **stack, unsigned int line_number)
 
 void _pchar(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 && (*stack)->n > 127)
+	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
